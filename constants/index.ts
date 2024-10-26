7ef83eafc0bd18ca0a900ee496bb66ec662bfb75
Boniife <anyanwuifeanyi3@gmail.com>
2024-10-26 19:22:51 +0100

    update

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+    {
+        label: 'Home',
+        route: '/',
+        imgUrl: '/icons/Home.svg'
+    },
+    {
+        label: 'Upcoming',
+        route: '/Upcoming',
+        imgUrl: '/icons/upcoming.svg'
+    },
+    {
+        label: 'Previous',
+        route: '/previous',
+        imgUrl: '/icons/previous.svg'
+    },
+    {
+        label: 'Recordings',
+        route: '/Recordings',
+        imgUrl: '/icons/Video.svg'
+    },
+    {
+        label: 'Personal Room',
+        route: '/Personal Room',
+        imgUrl: '/icons/add-personal.svg'
+    },
+]
