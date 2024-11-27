a3bb2e8c8290efb28cd9d50de10eafacaa27c81d
Boniife <anyanwuifeanyi3@gmail.com>
2024-11-27 10:31:42 +0100

    update

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -7,14 +7,15 @@ import { useUser } from '@clerk/nextjs'
 import { StreamCall, StreamTheme } from '@stream-io/video-react-sdk';
 import { useState } from 'react';
 import { Loader } from 'lucide-react';
+import Alert from '@/components/Alert';
 
 
 const Meeting = ({ params: {id} }: { params: { id: string } }) => {
   const {user, isLoaded} = useUser();
   const [isSetupComplete, setIsSetupComplete] = useState(false)
-  const { call, iscallLoading} = useGetCallbyid(id);
+  const { call, isCallLoading} = useGetCallbyid(id);
 
-  if(!isLoaded || iscallLoading) return <Loader />
+  if(!isLoaded || isCallLoading) return <Loader />
 
   if (!call) return (
     <p className="text-center text-3xl font-bold text-white">
@@ -22,9 +23,9 @@ const Meeting = ({ params: {id} }: { params: { id: string } }) => {
     </p>
   );
 
-  // const notAllowed = call.type === 'invited' && (!user || !call.state.members.find((m) => m.user.id === user.id));
+  const notAllowed = call.type === 'invited' && (!user || !call.state.members.find((m) => m.user.id === user.id));
 
-  // if (notAllowed) return <Alert title="You are not allowed to join this meeting" />;
+  if (notAllowed) return <Alert title="You are not allowed to join this meeting" />;
 
   return (
     <main className='h-screen w-full'>
